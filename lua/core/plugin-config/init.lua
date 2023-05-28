684f5ed9205be4863c77df2852fdd1e4ffe2ef5d
sumarokov-vp <sumarokov.vp@gmail.com>
2023-05-29 01:46:36 +0600

    init

---
+++ lua/core/plugin-config/init.lua
@@ -0,0 +1,7 @@
+require("core.plugin-config.gruvbox")
+require("core.plugin-config.lualine")
+require("core.plugin-config.nvim-tree")
+require("core.plugin-config.telescope")
+require("core.plugin-config.treesitter")
+require("core.plugin-config.bufferline")
+
