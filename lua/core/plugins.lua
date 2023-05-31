66232b722df50648850e973fdc357ed05be9d397
sumarokov-vp <sumarokov.vp@gmail.com>
2023-05-31 16:48:55 +0600

    123

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -30,13 +30,13 @@ local plugins = {
   'nvim-tree/nvim-tree.lua',
   'nvim-tree/nvim-web-devicons',
   {
-  "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    requires = { 
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    }
+    "nvim-neo-tree/neo-tree.nvim",
+      branch = "v2.x",
+      dependencies = { 
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+      }
   },
 
   'nvim-lualine/lualine.nvim',
