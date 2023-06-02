99a071fdb674198f48c80d7915c99b8d8ba8d616
sumarokov-vp <sumarokov.vp@gmail.com>
2023-06-02 09:39:38 +0600

    123

--- lua/core/plugin_config/bufferline.lua
+++ lua/core/plugin_config/bufferline.lua
@@ -1,6 +1,6 @@
 vim.opt.termguicolors = true
 require("bufferline").setup{}
 
-vim.keymap.set('n', '<S-h>', ':bprevious<CR>')
-vim.keymap.set('n', '<S-l>', ':bnext<CR>')
+vim.keymap.set('n', '<leader>h', ':BufferLineCyclePrev<CR>')
+vim.keymap.set('n', '<leader>l', ':BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<leader>w', ':w<CR> :BufferLineCyclePrev<CR> :BufferLineCloseRight<CR>')
